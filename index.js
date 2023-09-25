4d058f5421d86ca219d1b7a172b19df5f34f6133
NkoLow <104155407+NkoLow@users.noreply.github.com>
2023-09-24 20:02:30 -0700

    ASSIGNEMENT 2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Here is the fisrt assignement !")
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Dynamic endpoint that takes an argument
+app.get('/api/data/:arg', (req, res) => {
+    const arg = req.params.arg;
+    // Call a separate function to generate a JSON response
+    const result = generateResponse(arg);
+    res.json(result);
+});
+
+// Separate function for generating the JSON response
+function generateResponse(arg) {
+    // You can customize this function to generate a response based on 'arg'
+    return { message: `You requested data with argument: ${arg}` };
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ public/client.js
@@ -0,0 +1,23 @@
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const resultDiv = document.getElementById('result');
+
+button1.addEventListener('click', () => {
+    fetchData(1);
+});
+
+button2.addEventListener('click', () => {
+    fetchData(2);
+});
+
+function fetchData(arg) {
+    fetch(`/api/data/${arg}`)
+        .then((response) => response.json())
+        .then((data) => {
+            // Display the response in the 'result' div
+            resultDiv.textContent = JSON.stringify(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
